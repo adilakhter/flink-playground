c7d3bc03ba5f952ac4b5dc5fa35f87246bfb859d
Md. Adil Akhter <md.adilakhter@gmail.com>
2017-08-10 10:58:30 +0200

    renamed to joinined stream demo.

--- src/main/scala/org/apache/flink/quickstart/JoiningStreamDemo.scala
+++ src/main/scala/org/apache/flink/quickstart/JoiningStreamDemo.scala
@@ -7,22 +7,23 @@ import org.apache.flink.streaming.api.scala.{ConnectedStreams, DataStream, Joine
 import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
 import org.apache.flink.streaming.api.windowing.time.Time
 
-object ConnectedStreamDemo extends App {
+object JoiningStreamDemo extends App {
   val env = StreamExecutionEnvironment.getExecutionEnvironment
   env.setStreamTimeCharacteristic(TimeCharacteristic.IngestionTime)
   env.setParallelism(2)
 
-  implicit val t1 =  TypeInformation.of(classOf[Int])
-  implicit val t2 =  TypeInformation.of(classOf[(Customer,Int)])
-  implicit val t3 =  TypeInformation.of(classOf[Customer])
+  implicit val t1 = TypeInformation.of(classOf[Int])
+  implicit val t2 = TypeInformation.of(classOf[(Customer, Int)])
+  implicit val t3 = TypeInformation.of(classOf[Customer])
 
 
   import SimpleSource._
+
   val source1 = new ThrottlingSource[Customer](new SimpleSource[Customer], 10)
-  val source2 = new ThrottlingSource[Int](new SimpleSource[Int], 10)
+  val source2 = new ThrottlingSource[Int](new SimpleSource[Int], 1)
 
 
-  val firstInput: DataStream[Customer] =  env.addSource(source1)
+  val firstInput: DataStream[Customer] = env.addSource(source1)
   val secondInput: DataStream[Int] = env.addSource(source2)
 
   val firstKeyed = firstInput.keyBy(_.cardId)
@@ -37,8 +38,8 @@ object ConnectedStreamDemo extends App {
       .where((c: Customer) ⇒ c.cardId)
       .equalTo(identity)
       .window(TumblingEventTimeWindows.of(Time.seconds(3))).apply { (c, i) ⇒
-        (c, i)
-      }
+      (c, i)
+    }
 
   r.print().name("printer sink")
   env.execute()
